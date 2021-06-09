60b0d28a39729e16b7a5103cca737176856dd245
JuStPiNkCoDeR <sasha.los.0148@gmail.com>
2021-06-09 23:38:55 +0600

    feat: add camera follow logic

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,15 +8,47 @@ public class CameraFollow : MonoBehaviour
 {
     [Tooltip("The target object to follow.")]
     public Transform target;
-    // Start is called before the first frame update
-    void Start()
+
+    [Tooltip("The highest point where camera may locate.")]
+    public float topLimit = 10.0f;
+
+    [Tooltip("The lowest point where camera may locate.")]
+    public float bottomLimit = -10.0f;
+
+    [Tooltip("The following speed.")]
+    public float followSpeed = 0.5f;
+
+    // Set camera position after all object positions updating.
+    void LateUpdate()
     {
-        
+        // If target object exists...
+        if (target != null)
+        {
+            // Get its position
+            var newPosition = transform.position;
+            
+            // Work out where this camera should be
+            newPosition.y = Mathf.Lerp(newPosition.y, target.position.y, followSpeed);
+            
+            // Clamp this new location to within our limits
+            newPosition.y = Mathf.Min(newPosition.y, topLimit);
+            newPosition.y = Mathf.Max(newPosition.y, bottomLimit);
+            
+            // Update our location
+            transform.position = newPosition;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // When selected in the editor, draw a line from the top limit to the 
+    // bottom.
+    void OnDrawGizmosSelected()
     {
+        Gizmos.color = Color.yellow;
+
+        var position = transform.position;
+        var topPoint = new Vector3(position.x, topLimit, position.z);
+        var bottomPoint = new Vector3(position.x, bottomLimit, position.z);
         
+        Gizmos.DrawLine(topPoint, bottomPoint);
     }
 }
